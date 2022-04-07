e5a9494f8b1bf2bca6fd498fd4f240a3882005db
adjsky <igorlfmartins@mail.ru>
2022-04-07 23:51:19 +0300

    Configure barbar.nvim

--- nvim/lua/user/globals.lua
+++ nvim/lua/user/globals.lua
@@ -1,3 +1,8 @@
 vim.g.dracula_colorterm = 0
 vim.g.vim_json_conceal = 0
+vim.g.bufferline = {
+  animation = false,
+  closable = false,
+  insert_at_end = true
+}
 
--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -1,7 +1,7 @@
 local map = vim.api.nvim_set_keymap
 
 map("n", "<C-B>", "<cmd>CHADopen<cr>", { noremap = true })
-map("n", "<C-S>", "<cmd>Prettier<cr> <bar> :w<cr>", { noremap = true })
+map("n", "<C-S>", ":w<cr>", { noremap = true })
 
 map("n", "gd", "<Plug>(coc-definition)", { noremap = false })
 map("n", "gy", "<Plug>(coc-type-definition)", { noremap = false })
@@ -9,11 +9,28 @@ map("n", "gi", "<Plug>(coc-implementation)", { noremap = false })
 map("n", "gr", "<Plug>(coc-references)", { noremap = false })
 map("n", "<esc>", ":noh<cr>", { noremap = true })
 
+local bufferOpts = { noremap = true, silent = true }
+
+map("n", "g1", ":BufferGoto 1<CR>", bufferOpts)
+map("n", "g2", ":BufferGoto 2<CR>", bufferOpts)
+map("n", "g3", ":BufferGoto 3<CR>", bufferOpts)
+map("n", "g4", ":BufferGoto 4<CR>", bufferOpts)
+map("n", "g5", ":BufferGoto 5<CR>", bufferOpts)
+map("n", "g6", ":BufferGoto 6<CR>", bufferOpts)
+map("n", "g7", ":BufferGoto 7<CR>", bufferOpts)
+map("n", "g8", ":BufferGoto 8<CR>", bufferOpts)
+map("n", "g9", ":BufferGoto 9<CR>", bufferOpts)
+map("n", "g0", ":BufferLast<CR>", bufferOpts)
+
+map("n", "g.", ":BufferNext<CR>", bufferOpts)
+map("n", "g,", ":BufferPrevious<CR>", bufferOpts)
+map("n", "bc", ":BufferClose<CR>", bufferOpts)
+
 vim.cmd([[
   " use <tab> for trigger completion and navigate to the next complete item
   function! s:check_back_space() abort
-    let col = col('.') - 1
-    return !col || getline('.')[col - 1]  =~ '\s'
+    let col = col(".") - 1
+    return !col || getline(".")[col - 1]  =~ "\s"
   endfunction
 
   inoremap <silent><expr> <Tab>
