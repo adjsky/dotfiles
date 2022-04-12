4a39fe46baf8c7fc7cd1a6d7f93cf5adec69a157
adjsky <igorlfmartins@mail.ru>
2022-04-12 16:33:16 +0300

    Lint

--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -3,16 +3,16 @@ local lsp_installer = require'nvim-lsp-installer'
 local handlers = require'user.lsp.handlers'
 
 lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = handlers.on_attach,
-		capabilities = handlers.capabilities,
-	}
+  local opts = {
+    on_attach = handlers.on_attach,
+    capabilities = handlers.capabilities,
+  }
 
   if server.name == 'sumneko_lua' then
-	  local sumneko_opts = require('user.lsp.settings.sumneko')
-	  opts = vim.tbl_deep_extend('force', sumneko_opts, opts)
-	end
+    local sumneko_opts = require('user.lsp.settings.sumneko')
+    opts = vim.tbl_deep_extend('force', sumneko_opts, opts)
+  end
 
-	server:setup(opts)
+  server:setup(opts)
 end)
 
